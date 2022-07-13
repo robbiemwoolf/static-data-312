f507e61ba83430b626ab584f217b3a84517aff2b
Robbie Woolf <robbiemwoolf@gmail.com>
2022-07-13 09:35:13 -0700

    solution

--- src/app.js
+++ src/app.js
@@ -4,16 +4,47 @@ const app = express();
 const users = require("./data/users-data");
 const states = require("./data/states-data");
 
-// TODO: return an array of users from /users in form of { data: Array }
+// return a single user by id from /users/:userId in form of { data: Object }
+app.use("/users/:userId", (req, res, next) => {
+    const { userId } = req.params;
+    const foundUser = users.find((user) => user.id === Number(userId));
+  
+    if (foundUser) {
+        res.json({ data: foundUser });
+    } else {
+        next(`User ID not found: ${userId}`);
+    };
+});
 
-// TODO: return a single user by id from /users/:userId in form of { data: Object }
+// return an array of users from /users in form of { data: Array }
+app.use("/users", (req, res) => {
+    res.json({ data: users });
+});
 
-// TODO: return all states from /states in the form of { data: Array }
+// Return a single state from /states/:stateCode in the form of { data: { stateCode: String, name: String } }
+app.use('/states/:stateCode', (req, res, next) => {
+    const { stateCode } = req.params;
+    const state = states[stateCode];
+    if (state !== undefined) {
+        res.json({ data: { stateCode: stateCode, name:state }})
+    } else {
+        next(`State code not found: ${ stateCode }`)
+    };
+});
 
-// TODO: Return a single state from /states/:stateCode in the form of { data: { stateCode: String, name: String } }
+// return all states from /states in the form of { data: Array }
+app.use("/states", (req, res) => {
+    res.json({ data: states });
+});
 
-// TODO: add not-found handler
+// not-found handler
+app.use((req, res, next) => {
+    next(`Not found: ${req.originalUrl}`);
+});
 
-// TODO: Add error handler
+// error handler
+app.use((err, req, res, next) => {
+    res.send(err);
+});
 
 module.exports = app;
